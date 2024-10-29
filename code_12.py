20dc345bb5134f5bfa4c556d2b7fe7d5a96d4dd3
rvsam2714 <ricardo.sambrano@yahoo.com>
2024-10-29 16:26:58 -0600

    Implemented get_fibonacci_number
    Wrote and tested code for get_fibonacci_number function.

--- code_12.py
+++ code_12.py
@@ -1,8 +1,13 @@
 def get_fibonacci_number(position):
-    pass #Remove this line and insert your code here. Do not forget this function implements recursion.
+    if position == 0:
+        return 0
+    if position == 1:
+        return 1 
+    return get_fibonacci_number(position - 1) + get_fibonacci_number(position - 2)
+
 
 def get_fibonacci_number_sequence(number):
     pass #Remove this line and insert your code here. Do not forget to use get_fibonacci_number to create your list of numbers.
 
 if __name__ == "__main__":
-    pass #Remove this line and insert your code to test your Fibonacci function here
+    print(get_fibonacci_number(4))
