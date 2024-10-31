590c6444381e4d55cf1063dc45785d5408fe0819
rvsam2714 <ricardo.sambrano@yahoo.com>
2024-10-31 16:14:02 -0600

    testing code
    getting correct output first

--- code_12.py
+++ code_12.py
@@ -12,7 +12,16 @@ def get_fibonacci_number_sequence(number):
     if number == 1:
         return [0,1]
     start_sequence = [0,1]
-    
+    first = get_fibonacci_number(0)
+    second = get_fibonacci_number(1)
+    for i in range(2, number+1):
+        current = first + second
+        first = second
+        second = current
+        start_sequence.append(current)
+    return start_sequence
+
 
 if __name__ == "__main__":
-    print(get_fibonacci_number(4))
+    print(get_fibonacci_number(7))
+    print(get_fibonacci_number_sequence(7))
